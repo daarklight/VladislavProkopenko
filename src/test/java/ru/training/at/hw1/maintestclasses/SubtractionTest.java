d899e1b923d98f2af410f433c437cd25bd67dd73
Vlad <daark_light@mail.ru>
2021-06-17 22:44:00 +0300

    HW1 - 2nd review (+ minor changes)

--- src/test/java/ru/training/at/hw1/maintestclasses/SubtractionTest.java
+++ src/test/java/ru/training/at/hw1/maintestclasses/SubtractionTest.java
@@ -1,24 +1,14 @@
 package ru.training.at.hw1.maintestclasses;
 
-import com.epam.tat.module4.Calculator;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.training.at.hw1.DataProviders;
 
-public class SubtractionTest {
-
-    private Calculator calculator;
-
-    @BeforeClass(alwaysRun = true)
-    public void before() {
-        calculator = new Calculator();
-    }
+public class SubtractionTest extends CommonTestContent {
 
     @Test(dataProviderClass = DataProviders.class, dataProvider = "subtraction data - long",
           groups = "additionAndSubtraction")
-    public void subtractionTesting(long firstNum, long secondNum, long expectedResult) {
+    public void subtractionLongTesting(long firstNum, long secondNum, long expectedResult) {
         long actualResult = calculator.sub(firstNum, secondNum);
         Assert.assertEquals(actualResult, expectedResult);
         System.out.println(firstNum + " - " + secondNum + " = " + expectedResult);
@@ -26,14 +16,9 @@ public class SubtractionTest {
 
     @Test(dataProviderClass = DataProviders.class, dataProvider = "subtraction data - double",
           groups = "additionAndSubtraction")
-    public void subtractionTesting(double firstNum, double secondNum, double expectedResult) {
+    public void subtractionDoubleTesting(double firstNum, double secondNum, double expectedResult) {
         double actualResult = calculator.sub(firstNum, secondNum);
         Assert.assertEquals(actualResult, expectedResult, 0.0001);
         System.out.println(firstNum + " - " + secondNum + " = " + expectedResult);
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void after() {
-        calculator = null;
     }
 }
