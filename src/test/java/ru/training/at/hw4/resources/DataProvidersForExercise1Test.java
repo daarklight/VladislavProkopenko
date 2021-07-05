f36c2bad9715e855b2e6f1533fd94bb3b425ec70
daarklight <56450811+daarklight@users.noreply.github.com>
2021-07-06 00:04:40 +0300

    HW4 - 1st review.

--- src/test/java/ru/training/at/hw4/resources/DataProvidersForExercise1Test.java
+++ src/test/java/ru/training/at/hw4/resources/DataProvidersForExercise1Test.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Properties;
@@ -17,7 +18,7 @@ public class DataProvidersForExercise1Test {
         Properties properties = new Properties();
         File filePath = new File("src/test/resources/PageData.properties");
         FileInputStream fileInputStream = new FileInputStream(filePath);
-        properties.load(new InputStreamReader(fileInputStream, StandardCharsets.UTF_8));
+        properties.load(new InputStreamReader(fileInputStream, Charset.forName("windows-1252")));
 
         return new Object[][] {
             {properties.getProperty("mainPageURL"),
@@ -30,7 +31,7 @@ public class DataProvidersForExercise1Test {
                 List.of("To include good practices\nand ideas from successful\nEPAM project",
                     "To be flexible and\ncustomizable",
                     "To be multiplatform",
-                    "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more"),
+                    "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"),
                 properties.getProperty("iframeName"),
                 List.of("Home", "Contact form", "Service", "Metals & Colors", "Elements packs")
             },
