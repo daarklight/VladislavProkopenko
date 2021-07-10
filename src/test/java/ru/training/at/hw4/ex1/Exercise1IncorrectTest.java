0fe5bd9d58f6a6b335ddccf052b54140c36b856b
daarklight <56450811+daarklight@users.noreply.github.com>
2021-07-10 13:00:08 +0300

    Home work4 (#4)
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.
    
    * HW4 - 1st review.

---
+++ src/test/java/ru/training/at/hw4/ex1/Exercise1IncorrectTest.java
@@ -0,0 +1,59 @@
+package ru.training.at.hw4.ex1;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import java.util.List;
+import org.testng.annotations.Test;
+import ru.training.at.hw4.commons.PrepostConditions;
+import ru.training.at.hw4.resources.DataProvidersForExercise1Test;
+
+@Feature("TestNG is used as testing framework")
+public class Exercise1IncorrectTest extends PrepostConditions {
+
+    @Story("Incorrect Test for Exercise 1")
+    @Test(dataProviderClass = DataProvidersForExercise1Test.class, dataProvider = "Data for Exercise1Test")
+    public void testExercise1IncorrectTest(
+        String mainPageURL, String titleForMainPage, String username, String password,
+        String displayedUsername, List<String> expectedNamesOfHeaderMenuItems,
+        int expectedNumberOfTextFieldsAndImages, List<String> expectedBenefitTexts,
+        String iframeName, List<String> expectedNamesOfLeftMenuItems) {
+
+        // 1. Open test site by URL
+        stepsExercise1.openSite(mainPageURL);
+
+        // 2. Assert Browser title
+        stepsExercise1.checkIfBrowserTitleIsCorrect(titleForMainPage);
+
+        // 3. Perform login
+        stepsExercise1.performLogin(username, password);
+
+        // 4. Assert Username is logged in
+        stepsExercise1.checkDisplayedUsername(displayedUsername);
+
+        // 5. Assert that 4 items on the header section are displayed and have proper texts
+        stepsExercise1.assertHeaderItemsAreDisplayedAndHaveProperTexts(expectedNamesOfHeaderMenuItems);
+
+        // 6. Assert that there are 4 images on the Index Page and they are displayed
+        stepsExercise1.assertNumberOfImagesAndIfTheyAreDisplayed(10);
+
+        // 7. Assert that there are 4 texts on the Index Page under icons and they have proper text
+        // (According to teacher's recommendations, it was decided to exclude the requirement of
+        // searching these 4 texts under the icons. So, each text is searched as is, without
+        // any connections to points in space)
+        stepsExercise1.assertThatBenefitTextsExistAndHaveProperContent(expectedBenefitTexts);
+
+        // 8. Assert that the iframe with “Frame Button” exists
+        // (Cause step #9 checks whether “Frame Button” exists in the iframe,
+        // it was decided to check only existence of the frame)
+        stepsExercise1.assertExistenceOfIframe();
+
+        // 9. Switch to the iframe and check that there is “Frame Button” in the iframe
+        stepsExercise1.switchToIframeAndCheckExistenceOfIframeButton(iframeName);
+
+        // 10. Switch to original window back
+        stepsExercise1.switchToOriginalWindow();
+
+        // 11. Assert that 5 items in the Left Section are displayed and have proper text
+        stepsExercise1.assertThatLeftSectionItemsAreDisplayedAndHaveProperText(expectedNamesOfLeftMenuItems);
+    }
+}
---
+++ src/test/java/ru/training/at/hw4/steps/StepsExercise1.java
@@ -0,0 +1,76 @@
+package ru.training.at.hw4.steps;
+
+import io.qameta.allure.Step;
+import java.util.List;
+import org.openqa.selenium.WebDriver;
+import org.testng.asserts.SoftAssert;
+
+public class StepsExercise1 extends AbstractSteps {
+
+    public StepsExercise1(WebDriver webdriver) {
+        super(webdriver);
+    }
+
+    SoftAssert softAssert = new SoftAssert();
+
+    @Step("Open test site by URL")
+    public void openSite(String siteURL) {
+        commonPageContent.openPage(siteURL);
+    }
+
+    @Step("Assert Browser title")
+    public void checkIfBrowserTitleIsCorrect(String pageTitle) {
+        softAssert.assertEquals(commonPageContent.checkPageTitle(), pageTitle);
+    }
+
+    @Step("Perform login")
+    public void performLogin(String username, String password) {
+        loginFields.login(username, password);
+    }
+
+    @Step("Assert Username is logged in")
+    public void checkDisplayedUsername(String displayedUsername) {
+        loginFields.checkIfUsernameIsLoggedIn(displayedUsername);
+    }
+
+    @Step("Assert that 4 items on the header section are displayed and have proper texts")
+    public void assertHeaderItemsAreDisplayedAndHaveProperTexts(List<String> expectedNamesOfHeaderMenuItems) {
+        headerMenu.checkIfHeaderItemsAreDisplayed();
+        softAssert.assertEquals(headerMenu.transferListOfHeaderMenuItems(), expectedNamesOfHeaderMenuItems);
+    }
+
+    @Step("Assert that there are 4 images on the Index Page and they are displayed")
+    public void assertNumberOfImagesAndIfTheyAreDisplayed(int expectedNumberOfTextFieldsAndImages) {
+        bottomPicturesAndTexts.checkNumberOfImages(expectedNumberOfTextFieldsAndImages);
+        bottomPicturesAndTexts.checkIfAllImagesAreDisplayed();
+    }
+
+    @Step("Assert that there are 4 texts on the Index Page under icons and they have proper text")
+    public void assertThatBenefitTextsExistAndHaveProperContent(List<String> expectedBenefitTexts) {
+        softAssert.assertEquals(bottomPicturesAndTexts.transferListOfBenefitTexts(),
+            expectedBenefitTexts);
+    }
+
+    @Step("Assert that the iframe with “Frame Button” exists")
+    public void assertExistenceOfIframe() {
+        iframe.checkIfIframeExists();
+    }
+
+    @Step("Switch to the iframe and check that there is “Frame Button” in the iframe")
+    public void switchToIframeAndCheckExistenceOfIframeButton(String iframeName) {
+        iframe.switchToIframe(iframeName);
+        iframe.checkIfIframeButtonExists();
+    }
+
+    @Step("Switch to original window back")
+    public void switchToOriginalWindow() {
+        commonPageContent.switchToMainPage();
+    }
+
+    @Step("Assert that 5 items in the Left Section are displayed and have proper text")
+    public void assertThatLeftSectionItemsAreDisplayedAndHaveProperText(List<String> expectedNamesOfLeftMenuItems) {
+        leftMenu.checkIfLeftMenuItemsAreDisplayed();
+        softAssert.assertEquals(leftMenu.transferListOfLeftMenuItems(), expectedNamesOfLeftMenuItems);
+        softAssert.assertAll();
+    }
+}
